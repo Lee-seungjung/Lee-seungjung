4386759d4f639a2ce68e6c581697bb7940bbda86
leeseungjung <tmdwjd0295@naver.com>
2023-03-04 18:47:06 +0900

    nodejs 문자열

---
+++ nodejs/syntax/string.js
@@ -0,0 +1,5 @@
+console.log(1+1); //숫자
+console.log('1' + '1'); //문자, 따옴표나 쌍따옴표로 감싸줘야함
+
+//string.length 문자열 길이
+console.log('The World Wide Web (abbreviated WWW or the Web) is an information space whe'.length);
