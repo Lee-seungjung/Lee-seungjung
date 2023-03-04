538c645e433b854c1b84222ff460367e7dbfd810
leeseungjung <tmdwjd0295@naver.com>
2023-03-04 19:08:05 +0900

    변수의 활용

---
+++ nodejs/syntax/variable2.js
@@ -0,0 +1,12 @@
+var letter = 'Dear egoing he World Wide Web egoing(abbreviated WWW or the Web) is an information egoing space where documents a';
+console.log(letter);
+
+//긴 글 안에 egoing을 넣어둔 상태
+var name = 'egoing';
+var test = 'Dear ' + name +' he World Wide Web '+name+'(abbreviated WWW or the Web) is an information '+name+' space where documents a';
+console.log(test);
+
+
+
+
+
